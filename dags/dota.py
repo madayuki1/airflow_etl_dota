81cc65455a2c8660b122d3880c5bc3b6fd7a71d4
madayuki1 <abed.kurniawan76@gmail.com>
2024-08-08 19:49:18 +0800

    medallion architecture on postgres

--- dags/dota.py
+++ dags/dota.py
@@ -23,28 +23,55 @@ dag = DAG(
 def extract_heroes():
     etl_dota.extract('all_heroes_id')
 
+def extract_meta():
+    etl_dota.extract('current_pro_meta')
+
 def transform_heroes():
-    etl_dota.transform('all_heroes_id')
+    etl_dota.transform_heroes('all_heroes_id')
+
+def transform_meta():
+    etl_dota.transform_meta('current_pro_meta')
 
 def load_heroes():
     etl_dota.load('all_heroes_id')
 
-fetch_task = PythonOperator(
+def load_meta():
+    etl_dota.load('current_pro_meta')
+
+extract_heroes = PythonOperator(
     task_id='extract_heroes',
     python_callable=extract_heroes,
     dag=dag,
 )
 
-transform_task = PythonOperator(
+extract_meta = PythonOperator(
+    task_id='extract_meta', 
+    python_callable=extract_meta,
+    dag=dag,
+)
+
+transform_heroes = PythonOperator(
     task_id='transform_heroes',
     python_callable=transform_heroes,
     dag=dag,
 )
 
-load_task = PythonOperator(
+transform_meta = PythonOperator(
+    task_id='transform_meta',
+    python_callable=transform_meta,
+    dag=dag,
+)
+
+load_heroes = PythonOperator(
     task_id='load_heroes',
     python_callable=load_heroes,
     dag=dag,
 )
 
-fetch_task >> transform_task >> load_task
+load_meta = PythonOperator(
+    task_id='load_meta',
+    python_callable=load_meta,
+    dag=dag,
+)
+
+extract_heroes >> extract_meta >> transform_heroes >> transform_meta >> load_heroes >> load_meta
--- dags/etl_dota.py
+++ dags/etl_dota.py
@@ -2,13 +2,34 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 def extract(filename):
-    df = pd.read_csv(f'data/rawest/{filename}.csv')
-    df.to_csv(f'data/raw/{filename}.csv')
-
-def transform(filename):
     df = pd.read_csv(f'data/raw/{filename}.csv')
-    df_rename = df.rename(columns={'#': 'index', 'Name': "hero_name", 'Hero ID': 'hero_id'})
-    df_rename.to_csv(f'data/transformed/{filename}.csv')
+    engine = create_engine('postgres+psycopg2://postgres:postgres@postgres-data/postgres')
+    df.to_sql(f"bronze_{filename}", engine, if_exists='replace', index=False)
+    # df.to_csv(f'data/raw/{filename}.csv')
+
+def transform_heroes(filename):
+    # df = pd.read_csv(f'data/raw/{filename}.csv')
+    engine = create_engine('postgres+psycopg2://postgres:postgres@postgres-data/postgres')
+    df = pd.read_sql(f"bronze_{filename}", engine)
+    df_drop = df.drop('Unnamed: 0.1', axis=1)
+    df_rename = df_drop.rename(columns={'Unnamed: 0': 'index', 'Name': "hero_name", 'Hero ID': 'hero_id'})
+    df_rename.to_sql(f"silver_{filename}", engine, if_exists='replace', index=False)
+    # df_rename.to_csv(f'data/transformed/{filename}.csv')
+
+def transform_meta(filename):
+    # df = pd.read_csv(f'data/raw/{filename}.csv')
+    engine = create_engine('postgres+psycopg2://postgres:postgres@postgres-data/postgres')
+    df = pd.read_sql(f"bronze_{filename}", engine)
+    df_drop = df.drop('Unnamed: 0.1', axis=1)
+    df_rename = df_drop.rename(columns={'Unnamed: 0': 'index', 'Name': "hero_name", "Primary Attribute": "primary_attribute", \
+                                   "Attack Range": "attack_range", "Roles": "roles", "Total Pro wins": "total_pro_wins", \
+                                    "Times Picked": "times_picked", "Times Banned": "times_banned", "Win Rate": "win_rate"\
+                                        , "Niche Hero?": "niche_hero?"})
+    df_rename.to_sql(f"silver_{filename}", engine, if_exists='replace', index=False)
 
 def load(filename):
+    # df = pd.read_csv(f'/opt/airflow/data/transformed/{filename}.csv')
+    engine = create_engine('postgres+psycopg2://postgres:postgres@postgres-data/postgres')
+    df = pd.read_sql(f"silver_{filename}", engine)
+    df.to_sql(f"gold_{filename}", engine, if_exists='replace', index=False)
     return 
